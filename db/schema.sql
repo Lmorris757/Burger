9f84319c9c3ebc8af38be56a8cb8a37ba21c12e3
LxMO <lmorris757@outlook.com>
2018-12-21 08:38:49 -0800

    1st

---
+++ db/schema.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE burgers_db;
+
+USE burger_db;
+
+CREATE TABLE burgers(
+    id INT NOT NULL AUTO_INCREMENT,
+    burger_name VARCHAR(255),
+    devoured VARCHAR(255)
+);
---
+++ db/seeds.sql
@@ -0,0 +1,9 @@
+INSERT INTO burgers (burger_name) VALUES ('Veggie Burger');
+
+INSERT INTO burgers (burger_name) VALUES ('Salmon Burger');
+
+INSERT INTO burgers (burger_name) VALUES ('KobeBeef Burger');
+
+INSERT INTO burgers (burger_name) VALUES ('Cheese Burger');
+
+INSERT INTO burgers (burger_name) VALUES ('Chicken Burger');
